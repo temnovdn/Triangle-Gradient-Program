61a04d1ba276140f89a6ccd250c6a146268c4550
Dmitry Temnov <temnov@inetra.ru>
2016-11-16 14:22:13 +0700

    Added apexes check in triangle, GUI finished

--- src/draw.py
+++ src/draw.py
@@ -1,6 +1,76 @@
 from gradient import *
 from point import Point
+from Tkinter import *
 
-triangle = TriangleGradient(Point(0,0), Point(100, 100), Point(200, 100))
-for point in triangle.internal_points:
-    print (point.red, point.green, point.blue)
+
+def draw_triangle():
+    red_x = IntVar()
+    red_x = red_x_input.get()
+    red_y = IntVar()
+    red_y = red_y_input.get()
+
+    green_x = IntVar()
+    green_x = green_x_input.get()
+    green_y = IntVar()
+    green_y = green_y_input.get()
+
+    blue_x = IntVar()
+    blue_x = blue_x_input.get()
+    blue_y = IntVar()
+    blue_y = blue_y_input.get()
+    try:
+        gradient = TriangleGradient(Point(int(red_x), int(red_y)), Point(int(green_x), int(green_y)), Point(int(blue_x), int(blue_y)))
+        draw_area.blank()
+
+        for point in gradient.internal_points:
+            draw_area.put("#%02x%02x%02x" % (point.red, point.green, point.blue), (point.x, point.y))
+
+        label = Label(master=root, image=draw_area)
+        label.pack(anchor=W)
+    except ValueError:
+        label = Label(master=apex_coordinates_input, text="Input values could not be apexes of triangle")
+        label.pack(anchor=N)
+
+window_width = 900
+window_height = 1100
+text_area_width = 20
+
+root = Tk()
+root.title('Triangle gradient')
+root.geometry('{0!s}x{1!s}+50+50'.format(window_height, window_width))
+
+draw_area = PhotoImage(width=window_width-text_area_width, height=window_height-20)
+
+apex_coordinates_input = Frame(master=root, width=text_area_width, height=window_height)
+apex_coordinates_input.pack(anchor=E)
+
+coordinate_frames_height = window_height // 3
+red_coordinates = Frame(master=apex_coordinates_input, width=text_area_width, height=coordinate_frames_height)
+red_coordinates.pack()
+
+green_coordinates = Frame(master=apex_coordinates_input, width=text_area_width, height=coordinate_frames_height)
+green_coordinates.pack()
+
+blue_coordinates = Frame(master=apex_coordinates_input, width=text_area_width, height=coordinate_frames_height)
+blue_coordinates.pack()
+
+red_x_input = Entry(master=red_coordinates, bg='red', fg='white', width=10)
+red_x_input.pack(side=LEFT)
+red_y_input = Entry(master=red_coordinates, bg='red', fg='white', width=10)
+red_y_input.pack(side=RIGHT)
+
+green_x_input = Entry(master=green_coordinates, bg='green', fg='white', width=10)
+green_x_input.pack(side=LEFT)
+green_y_input = Entry(master=green_coordinates, bg='green', fg='white', width=10)
+green_y_input.pack(side=RIGHT)
+
+blue_x_input = Entry(master=blue_coordinates, bg='blue', fg='white', width=10)
+blue_x_input.pack(side=LEFT)
+blue_y_input = Entry(master=blue_coordinates, bg='blue', fg='white', width=10)
+blue_y_input.pack(side=RIGHT)
+
+draw_button = Button(master=apex_coordinates_input, text="Draw!", command=draw_triangle)
+draw_button.pack()
+
+
+mainloop()
--- src/gradient.py
+++ src/gradient.py
@@ -3,6 +3,24 @@ from point import Point
 from math import sqrt
 
 
+def spline_length(point1, point2):
+    """spline_length(dict,dict) -> float
+    Return the length of spline between point1 and point2.
+    """
+    return sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2)
+
+
+def triangle_apexes(point1, point2, point3):
+    """is_triangle(Point,Point) -> bool
+    Return true if point set can be triangle apexes, return false otherwise.
+    """
+    side1 = spline_length(point1, point2)
+    side2 = spline_length(point1, point3)
+    side3 = spline_length(point2, point3)
+
+    return (side1 < side2 + side3) and (side2 < side1 + side3) and (side3 < side1 + side2)
+
+
 class TriangleGradient:
     def __init__(self, red_apex, green_apex, blue_apex):
         """
@@ -10,6 +28,10 @@ class TriangleGradient:
         :param Point green_apex:
         :param Point blue_apex:
         """
+        if not triangle_apexes(red_apex, green_apex, blue_apex):
+            raise ValueError("Points (%s,%s),(%s,%s),(%s,%s) can not be triangle apexes" %
+                             (red_apex.x, red_apex.y, green_apex.x, green_apex.y, blue_apex.x, blue_apex.y))
+
         self.red_apex = red_apex
         self.green_apex = green_apex
         self.blue_apex = blue_apex
@@ -36,8 +58,8 @@ class TriangleGradient:
                 point = Point(x, y)
 
                 a = (self.red_apex.x - point.x) * (self.green_apex.y - self.red_apex.y) - (self.green_apex.x - self.red_apex.x) * (self.red_apex.y - point.y)
-                b = (self.green_apex.x - point.x) * (self.blue_apex.y - self.green_apex.y) - (self.blue_apex.x - self.green_apex.x) * (self.blue_apex.y - point.y)
-                c = (self.blue_apex.x - point.x) * (self.red_apex.y - self.blue_apex.y) - (self.red_apex.x - self.blue_apex.x) * (self.green_apex.y - point.y)
+                b = (self.green_apex.x - point.x) * (self.blue_apex.y - self.green_apex.y) - (self.blue_apex.x - self.green_apex.x) * (self.green_apex.y - point.y)
+                c = (self.blue_apex.x - point.x) * (self.red_apex.y - self.blue_apex.y) - (self.red_apex.x - self.blue_apex.x) * (self.blue_apex.y - point.y)
 
                 if (a >= 0 and b >= 0 and c >= 0) or (a <=0 and b <= 0 and c <= 0):
                     point.red = int(round((spline_length(self.red_apex, point) / self.red_scale) * 255, 0))
@@ -46,10 +68,3 @@ class TriangleGradient:
                     points.append(point)
 
         return points
-
-
-def spline_length(point1, point2):
-    """spline_length(dict,dict) -> float
-    Return the length of spline between point1 and point2.
-    """
-    return sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2)
